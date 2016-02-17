bccb11d11dd9f108b71a1864bbec916cd2afa860
zeichenjoyce <zeichenjoyce@gmail.com>
2016-02-17 16:57:39 +0800

    rails g migration add_user_id_to_post user_id:integer
    rails g migration add_user_id_to_post user_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217065301) do
+ActiveRecord::Schema.define(version: 20160217085610) do
 
   create_table "group_users", force: :cascade do |t|
     t.integer  "group_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160217065301) do
     t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
